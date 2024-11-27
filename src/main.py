e7053bb1aad654f0b3a84375e062cdcca2e12f51
Hans-Christian Knolle <hanscknolle08@gmail.com>
2024-11-27 08:54:00 +0100

    Edited pyproject.toml and fixed the main.py test script

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from Themen.Turtle.main import Drawing
 
 def main():
-    Turtle = Drawing()
+    Turtle = Drawing(speed=0, shape="turtle", pensize=10, color="red")
     Turtle.n_eck(20)
 
 if __name__ == '__main__':
     try:
         main()
-    except Exception:
+    except Exception as e:
         print("Programm beendet")
+        print(e)
